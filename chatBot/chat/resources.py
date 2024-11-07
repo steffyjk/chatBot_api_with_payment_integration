c552e338513b21cf3ecc706fef38f27afdc1ab52
steffyjk <steffy.jk2018@gmail.com>
2024-11-07 22:28:22 +0530

    minor change

--- chatBot/chat/resources.py
+++ chatBot/chat/resources.py
@@ -6,5 +6,9 @@ from chatBot.chat import services
 class chatCommunicationView(MethodView):
 
     def post(self):
+        """Get Chat Response as per Provided request.message
+        Returns:
+            _type_: Json [ reply of question ]
+        """
         return services.get_chat_response(request)
     
--- chatBot/chat/routes.py
+++ chatBot/chat/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from chatBot.chat.resources import chatCommunicationView
 
+# Declare The BluePrint of chat Module
 chat = Blueprint('chat', __name__)
 
 chat.add_url_rule('/chat', view_func=chatCommunicationView.as_view('chat_view'))
